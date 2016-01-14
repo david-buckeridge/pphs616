fc719bb2a4dc982519f801cd94c5b267a60ca3a1
david-buckeridge <david.buckeridge@mcgill.ca>
2016-01-14 08:43:58 -0500

    An initial commit

---
+++ software01.R
@@ -0,0 +1,13 @@
+# A program to illustrate some basic features in R
+
+# Load the gapminder date set from https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv
+
+data.url = "https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv"
+gapminder = read.csv(data.url)
+
+# Compute group mean of Life Expectancy by Country
+# Using aggregate
+
+
+# Using tapply
+
