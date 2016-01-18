ad0ec76bab025f76dcdd21e36413185b032d0f07
David Buckeridge <david.buckeridge@mcgill.ca>
2016-01-18 06:39:35 -0500

    Completing example program

--- software01.R
+++ software01.R
@@ -1,13 +1,20 @@
-# A program to illustrate some basic features in R
+# A program to illustrate some basic features in R for pphs 616
+# By David Buckeridge
+
+# Declare and load libraries
+library(ggplot2)
 
 # Load the gapminder date set from https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv
-
 data.url = "https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv"
 gapminder = read.csv(data.url)
 
 # Compute group mean of Life Expectancy by Country
-# Using aggregate
+## Using aggregate
+le = aggregate(lifeExp ~ country, data = gapminder, mean)
+gdp = aggregate(gdpPercap ~ country, data = gapminder, mean)
+plot.data = data.frame(country = gdp$country, gdp = gdp$gdpPercap, le = le$lifeExp)
 
 
-# Using tapply
-
+# Plot life expectancy by gdp for each country
+ggplot(data = plot.data, mapping = aes(x=le, y=gdp)) +
+  geom_point()
