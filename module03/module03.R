83720862fa8db8babc33eb0384ca417a7706e140
david-buckeridge <david.buckeridge@mcgill.ca>
2016-03-07 13:18:15 -0500

    Initial code for Module 3

---
+++ module03/module03.R
@@ -0,0 +1,182 @@
+library(maptools)
+library(spdep)
+library(classInt)
+library(RColorBrewer)
+
+fsa.shp.name = "data/Montreal_FSA_2001/Montreal_FSA_Census_2001_MTM"
+fsa.visit.name = "data/montreal_ili_visits.csv"
+
+# read shapefile
+fsa.shp = readShapePoly(fsa.shp.name, IDvar='FSA')
+# summary(fsa.shp)
+# attributes(fsa.shp@data)
+
+# read visits file
+fsa.visits = read.table(fsa.visit.name, header=TRUE, sep=",")
+# summary(fsa.visits)
+
+# create a plot of FSA boundaries in Montreal
+plot(fsa.shp)
+
+
+## Plot median family income as choropleth map
+# define number of classes in plot
+nclr = 5
+# define color palette -- see http://casoilresource.lawr.ucdavis.edu/drupal/node/192
+#  for a nice listing of the color palettes
+plotclr = brewer.pal(nclr,"Greens")
+# define the classes or breaks for the data
+class = classIntervals(fsa.shp@data$Median_Fam, nclr, style="quantile", dataPrecision=0)
+# create a vector of colors for each region
+colcode = findColours(class,plotclr)
+
+# plot the region boundaries
+plot(fsa.shp)
+# add the colors, title and legend
+plot(fsa.shp, col=colcode, add=T)
+title(sub="Median Family Income", main='Classed Choropleth')
+legend('topleft', legend=names(attr(colcode, "table")), fill=attr(colcode,"palette"), cex=0.9, bty='n')
+
+# now create a proportional symbol or 'bubble map' for the same data
+max.symbol.size=5
+min.symbol.size=1
+plotvar = fsa.shp@data$Median_Fam
+symbol.size = ((plotvar-min(plotvar))/(max(plotvar)-min(plotvar))*(max.symbol.size-min.symbol.size) + min.symbol.size)
+
+plot(fsa.shp)
+mtl.cntr = coordinates(fsa.shp)
+points(mtl.cntr, pch=16, col=colcode, cex=symbol.size)
+points(mtl.cntr, cex=symbol.size)
+title(sub="Median Family Income", main="Bubble Plot")
+legend('topleft', legend=names(attr(colcode, "table")), fill=attr(colcode,"palette"), cex=0.9, bty='n')
+
+
+
+## Plot Crude ILI Admission Rates 
+# Join my data with data from shapfile - we do this to ensure correct linkage...
+ili = data.frame(fsa=fsa.shp@data$FSA, pop=fsa.shp@data$POP96)
+ili = merge(fsa.visits, ili)
+# note that the 3-year counts are divided by 3 to approximate an annual rate
+ili$rate = (ili$visits / 3) / ili$pop
+
+nclr = 5
+plotclr = brewer.pal(nclr, 'Reds')
+class.crude = classIntervals((ili$rate*1000), nclr, style='quantile', dataPrecision=0)
+colcode.crude = findColours(class.crude, plotclr)
+
+plot(fsa.shp)
+plot(fsa.shp, col=colcode.crude, add=T)
+title(sub="Crude Rates of ILI Visits by FSA (Annual Visits per 1,000)")
+legend('topleft', legend=names(attr(colcode.crude, "table")), fill=attr(colcode.crude,"palette"), cex=0.9, bty='n')
+
+
+# Plot adjusted rates?
+# Create map of own values?
+
+
+## Build connectivity matrix
+# Use triangulation to build connectivity
+fsa.nb.tri = tri2nb(coordinates(fsa.shp), row.names=fsa.shp@data$FSA)
+summary(fsa.nb.tri)
+# What are the least and most connected regions? Verify these regions on the map, does they make sense?
+
+plot(fsa.shp, border='darkgrey', las=1)
+plot(fsa.nb.tri, coordinates(fsa.shp), add=TRUE)
+# Does the plot correspond to your idea of connectivity? Why?
+
+
+# Use nearest neighbors to build connectivity
+k1 = knn2nb(knearneigh(coordinates(fsa.shp)))
+max.distance = max(unlist(nbdists(k1, coordinates(fsa.shp))))
+fsa.nb.knn = dnearneigh(coordinates(fsa.shp), 0, max.distance, fsa.shp@data$FSA)
+summary(fsa.nb.knn)
+
+plot(fsa.shp, border='darkgrey', las=1)
+plot(fsa.nb.knn, coordinates(fsa.shp), add=TRUE)
+# Does the plot correspond to your idea of connectivity? Why?
+
+# try with topology of shape file
+fsa.polys = polygons(fsa.shp)
+fsa.nb.poly = poly2nb(fsa.polys, queen=FALSE)
+summary(fsa.nb.poly)
+
+plot(fsa.shp, border='darkgrey', las=1)
+plot(fsa.nb.poly, coordinates(fsa.shp), add=TRUE)
+# Does the plot correspond to your idea of connectivity? Why?
+
+# Read cleaned nb file (essentially the toplogy file with some manual editing...)
+fsa.nb = read.gal('data/fsa.nb.clean')
+summary(fsa.nb)
+
+plot(fsa.shp, border='darkgrey', las=1)
+plot(fsa.nb, coordinates(fsa.shp), col="red", add=TRUE)
+plot(fsa.nb.poly, coordinates(fsa.shp), add=TRUE)
+
+
+
+## Estimate EBE Local Rates
+
+ili.ebe = EBlocal(ili$visits/3, ili$pop, fsa.nb)
+ili.ebe[1:10,]
+hist(ili.ebe$est/ili.ebe$raw*100)
+# Which values changed the most? Why?
+
+# Use Ebest to peform global smoothing. Is there any difference? Explain.
+
+nclr = 5
+plotclr = brewer.pal(nclr, 'Reds')
+class.raw = classIntervals(round(ili.ebe$raw*1000,0), nclr, style='quantile')
+
+colcode.raw = findColours(class.raw, plotclr)
+colcode.est = findColours(class.raw, plotclr)
+
+plot(fsa.shp)
+plot(fsa.shp, col=colcode.raw, add=T)
+title(sub="Raw Rates of ILI Visits (Annual Visits per 1,000)")
+legend('topleft', legend=names(attr(colcode.raw, "table")), fill=attr(colcode.raw,"palette"), cex=0.9, bty='n')
+
+plot(fsa.shp)
+plot(fsa.shp, col=colcode.est, add=T)
+title(sub="EBE (Local) Smoothed Rates of ILI Visits (Annual Visits per 1,000)")
+legend('topleft', legend=names(attr(colcode.est, "table")), fill=attr(colcode.est,"palette"), cex=0.9, bty='n')
+
+
+## Estimate Moran's I
+# examine and transform data as needed
+hist(ili.ebe$raw)
+hist(sqrt(ili.ebe$raw))
+hist(fsa.shp@data$Median_Fam)
+
+# create row-standardized weight matrix
+fsa.w = nb2listw(fsa.nb)
+str(fsa.w)
+
+(ili.moran = moran.test(sqrt(ili.ebe$raw), fsa.w))
+(income.moran = moran.test(fsa.shp@data$Median_Fam, fsa.w))
+
+moran.plot(sqrt(ili.ebe$raw), fsa.w, labels=fsa.visits$fsa)
+moran.plot(fsa.shp@data$Median_Fam, fsa.w, labels=fsa.visits$fsa)
+
+
+# plot local Moran's I values
+ili.moran.local = localmoran(sqrt(ili.ebe$raw), fsa.w)
+income.moran.local = localmoran(fsa.shp@data$Median_Fam, fsa.w)
+
+nclr = 5
+plotclr = brewer.pal(nclr, 'Reds')
+class.ili = classIntervals(ili.moran.local[,1], nclr, style='quantile')
+class.income = classIntervals(income.moran.local[,1], nclr, style='quantile')
+
+colcode.ili = findColours(class.ili, plotclr)
+colcode.income = findColours(class.income, plotclr)
+
+
+plot(fsa.shp)
+plot(fsa.shp, col=colcode.ili, add=T)
+title(sub="Local Moran's I for ILI")
+legend('topleft', legend=names(attr(colcode.ili, "table")), fill=attr(colcode.ili,"palette"), cex=0.9, bty='n')
+
+plot(fsa.shp)
+plot(fsa.shp, col=colcode.income, add=T)
+title(sub="Local Moran's I for Income")
+legend('topleft', legend=names(attr(colcode.income, "table")), fill=attr(colcode.income,"palette"), cex=0.9, bty='n')
