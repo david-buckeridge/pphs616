405f2ac14ce25a5485c66ccdc806d7256f12302c
david-buckeridge <david.buckeridge@mcgill.ca>
2020-03-10 08:08:16 -0400

    Updates to COVID-19

--- moduleCOVID-19/moduleCOVID-19.R
+++ moduleCOVID-19/moduleCOVID-19.R
@@ -1,7 +1,8 @@
-## ------------- Module COVID-19 Starter -------------
+## ------------- Module COVID-19 -------------
 
 ## ------------- Load Libraries ----------------------
-
+library(RColorBrewer)
+library(coarseDataTools)
 
 ## ------------- Load Data ---------------------------
 
@@ -43,56 +44,53 @@ jhu.n.country = length(jhu.country.region)
 country.counts = vector(mode="list", length=jhu.n.country)
 names(country.counts) = jhu.country.region
 
-# Function to aggregate case counts across regions in a country, if necessary
-aggregateJHUCounts <- function(x) {
-  if (nrow(x) > 1) {
-    # if multiple rows for a country, aggregate case counts to form single row
-    counts = rowsum(x[,5:ncol(x)], group=rep(1, nrow(x)))
-  } else {
-    # otherwise use single set of counts
-    counts = x[,5:ncol(x)]
-  }   
-  return(counts)
-} # aggregateJHUCounts
 
-
-# extract from jhu.cases, vector of cases by day from 2020-1-22 to most recent update (2020-03-06)
+# extract from jhu.cases, vector of cases by day from 2020-1-22 to most recent update
 # for country in country.region
-for (country.region in jhu.country.region) {
-  this.country.cases = aggregateJHUCounts(jhu.cases[jhu.cases$Country.Region == country.region,])
-  this.country.deaths = aggregateJHUCounts(jhu.deaths[jhu.deaths$Country.Region == country.region,])
-  this.country.recovered = aggregateJHUCounts(jhu.recovered[jhu.deaths$Country.Region == country.region,])
+for (country in jhu.country.region) {
+  country.cases = aggregateJHUCounts(jhu.cases[jhu.cases$Country.Region == country,])
+  country.deaths = aggregateJHUCounts(jhu.deaths[jhu.deaths$Country.Region == country,])
+  country.recovered = aggregateJHUCounts(jhu.recovered[jhu.deaths$Country.Region == country,])
   
   # place daily counts in list, indexed by country
-  country.counts[[country.region]] = list("country" = country.region, 
-                                          "cases"= this.country.cases, 
-                                          "deaths" = this.country.deaths,
-                                          "recovered" = this.country.recovered)
-} # for jhu.country.region
+  country.counts[[country.region]] = list("country" = country, 
+                                          "cases"= country.cases, 
+                                          "deaths" = country.deaths,
+                                          "recovered" = country.recovered)
+} # for country
+
+
+# TODO: create function for calculating incident events from cumulative series, then add the
+#       incident series to country.counts as new indicators.
+
+
+###### NOT WORKING
+# Create long data structure for plotting time series 
+# country, date, indicator, value
+indicator.types = c("cases", "deaths", "recovered") # order must reflect order in country.counts objects
+col.dim = 4 # the columns above
+row.dim = jhu.n.country * jhu.days * length(indicator.types)
+country.counts.long = data.frame(country=rep(jhu.country.region,length.out=row.dim), 
+                                date=rep(jhu.dates,length.out=row.dim),
+                                indicator=rep(indicator.types, length.out=row.dim), 
+                                value=rep(NA,row.dim))
+
+for (country.region in jhu.country.region) {
+  country.object = country.counts[[country.region]]
+  for (indicator in indicator.types) {
+    index.type = which(indicator.types == indicator)
+    country.series = country.object[[index.type+1]]
+    
+    country.counts.long[country.counts.long$country == country.region &
+                        country.counts.long$indicator == indicator,4] = as.vector(country.series)
+    
+  } # indicator
+} # country
+
+
 
 
 ## ------------ Extract information about each Country from time series ------
-# Function to be used in lapply to extract maximum case count (should be last value..)
-# We create the index knowing first entry is country name and second entry is list, containing
-#  country name (to ensure integrity) in first slot, and vectors of integer counts for cases,
-#  deaths, and recovered in subsequent slots in that order.
-current.count <- function(x, count.type) {
-  valid.types = c("cases","deaths","recovered")
-  index.type = which(valid.types == count.type)
-  
-  return(max(x[[(index.type + 1)]]))
-}
-
-# Function to return the day of the first event of the type requested
-#  (as 1-indexed integer days from jhu.start)
-first.event <- function(x, event.type) {
-  valid.types = c("cases","deaths","recovered")
-  index.type = which(valid.types == event.type)
-  
-  index.events = which(x[[index.type + 1]] > 0)
-  
-  return(ifelse(length(index.events) > 0, min(index.events), NA))
-}
 
 # Apply functions to extract summary metrics
 country.totalCases = lapply(X=country.counts, FUN=current.count, "cases")
@@ -115,6 +113,11 @@ for (country.region in jhu.country.region) {
   country.indicators$firstDeath[country.indicators$country == country.region] = country.firstDeath[[country.region]]
 }
 
+# Create long data structure for visualizing indicator data
+
+
+
+
 
 
 ## ------------------ Plot time-series --------------------------
@@ -133,6 +136,19 @@ country.cases.long = data.frame(country=NULL, date=NULL, count=NULL)
 ## ------------------ Estimate Case Fatility Risk ---------------
 # estimate case fatility risk by country using package by Reich and see article by Lipsitch
 
+# create data set for input to EMforCFR function with the following columns:
+# grp a 1 or a 2 indicating which of the two groups, j, the observation is for.
+# new.times an integer value representing the time, t, of observation.
+# R the count of recovered cases with onset at time t in group j.
+# D the count of deaths which occurred at time t in groupo j (note that these deaths did not have disease onset at time t but rather died at time t).
+# N the total cases at t, j, or the sum of R and D columns.
+
+
+# start with second day
+
+# for all days
+  # subtract value for previous day from value for today to obtain new events today 
+
 
 
 ## ------------------ Is COVID-19 Controllable ------------------
@@ -142,4 +158,40 @@ country.cases.long = data.frame(country=NULL, date=NULL, count=NULL)
 
 ## ------------------ Something else! ---------------------------
 
+
+## ------------------ Functions ---------------------
+# Function to aggregate case counts across regions in a country, if necessary
+aggregateJHUCounts <- function(x) {
+  if (nrow(x) > 1) {
+    # if multiple rows for a country, aggregate case counts to form single row
+    counts = rowsum(x[,5:ncol(x)], group=rep(1, nrow(x)))
+  } else {
+    # otherwise use single set of counts
+    counts = x[,5:ncol(x)]
+  }   
+  return(counts)
+} # aggregateJHUCounts
+
+
+# Function to be used in lapply to extract maximum case count (should be last value..)
+# We create the index knowing first entry is country name and second entry is list, containing
+#  country name (to ensure integrity) in first slot, and vectors of integer counts for cases,
+#  deaths, and recovered in subsequent slots in that order.
+current.count <- function(x, count.type) {
+  valid.types = c("cases","deaths","recovered")
+  index.type = which(valid.types == count.type)
+  
+  return(max(x[[(index.type + 1)]]))
+}
+
+# Function to return the day of the first event of the type requested
+#  (as 1-indexed integer days from jhu.start)
+first.event <- function(x, event.type) {
+  valid.types = c("cases","deaths","recovered")
+  index.type = which(valid.types == event.type)
+  
+  index.events = which(x[[index.type + 1]] > 0)
+  
+  return(ifelse(length(index.events) > 0, min(index.events), NA))
+}
 
